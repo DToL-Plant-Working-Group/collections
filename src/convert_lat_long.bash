7106eea80128fef0dc289125758999af9a46e08d
Max Brown <mb39@sanger.ac.uk>
2021-10-18 09:58:12 +0100

    Update data on 2021-10-18.

--- src/convert_lat_long.bash
+++ src/convert_lat_long.bash
@@ -5,7 +5,8 @@ for i in $lat_lon; do
     first=$(echo $i | cut -d, -f1)
     second=$(echo $i | cut -d, -f2)
     
-    printf "$first\t$second\t"
-    ./geodojo_county --lat $first --lon $second
+    parsed_county=$(./geodojo_county --lat $first --lon $second)
+    
+    printf "$first\t$second\t$parsed_county\n"
     
 done
