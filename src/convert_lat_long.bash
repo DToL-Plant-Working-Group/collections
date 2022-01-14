e1b21bfb07c50b2f8d7c19404045c86eaf310e78
Max Brown <mb39@sanger.ac.uk>
2022-01-14 17:01:41 +0000

    Update data on 2022-01-14.

--- src/convert_lat_long.bash
+++ src/convert_lat_long.bash
@@ -1,12 +1,16 @@
 
-lat_lon=$(cut -d, -f6,7 $1 | tail -n +2 | sort | uniq)
+cut -d, -f6,7 $1 | tail -n +2 | sort | uniq > temp_lat_lons.txt
 
-for i in $lat_lon; do
-    first=$(echo $i | cut -d, -f1)
-    second=$(echo $i | cut -d, -f2)
-    
-    parsed_county=$(./geodojo_county --lat $first --lon $second)
-    
-    printf "$first\t$second\t$parsed_county\n"
-    
-done
+./geodojo_county -f temp_lat_lons.txt
+
+rm temp_lat_lons.txt
+
+# for i in $lat_lon; do
+#     first=$(echo $i | cut -d, -f1)
+#     second=$(echo $i | cut -d, -f2)
+
+#     parsed_county=$(./geodojo_county --lat $first --lon $second)
+
+#     printf "$first\t$second\t$parsed_county\n"
+
+# done
